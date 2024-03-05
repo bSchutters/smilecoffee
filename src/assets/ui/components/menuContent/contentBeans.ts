639d64b76ac726d6d12b732bdee0c892a590e672
bSchutters <bryan021@hotmail.be>
2024-03-05 20:31:18 +0100

    mapping products

---
+++ src/assets/ui/components/menuContent/contentBeans.ts
@@ -0,0 +1,35 @@
+import pikePlace  from "../../../img/pikePlace.jpeg";
+
+export const contentBeans = [
+    {
+        title: "Pike Place",
+        price: 2,
+        img: pikePlace,
+    },
+    {
+        title: "Pike Place",
+        price: 2,
+        img: pikePlace,
+    },
+    {
+        title: "Pike Place",
+        price: 2,
+        img: pikePlace,
+    },
+    {
+        title: "Pike Place",
+        price: 2,
+        img: pikePlace,
+    },
+    {
+        title: "Pike Place",
+        price: 2,
+        img: pikePlace,
+    },
+    {
+        title: "Pike Place",
+        price: 2,
+        img: pikePlace,
+    },
+    
+]
--- src/assets/ui/components/menuContent/menuContentBeans.tsx
+++ src/assets/ui/components/menuContent/menuContentBeans.tsx
@@ -1,27 +1,17 @@
 import { Product } from "./product/product";
+import { contentBeans } from "./contentBeans";
 
 export default function MenuContentBeans() {
-  let urlImg =
-    "https://www.starbucks.fr/sites/starbucks-fr/files/styles/c04_image_text_grid_600x600/public/2020-07/Pike-Place-Coffee-Bag-C04-RESIZED.jpg.webp?itok=2iLDjmBl";
-
   return (
     <div className="grid grid-cols-4 gap-30">
-      <Product title="Test1" price={2} img={urlImg} />
-      <Product title="Test1" price={2} img={urlImg} />
-      <Product title="Test1" price={2} img={urlImg} />
-      <Product title="Test1" price={2} img={urlImg} />
-      <Product title="Test1" price={2} img={urlImg} />
-      <Product title="Test1" price={2} img={urlImg} />
-      <Product title="Test1" price={2} img={urlImg} />
-      <Product title="Test1" price={2} img={urlImg} />
-      <Product title="Test1" price={2} img={urlImg} />
-      <Product title="Test1" price={2} img={urlImg} />
-      <Product title="Test1" price={2} img={urlImg} />
-      <Product title="Test1" price={2} img={urlImg} />
-      <Product title="Test1" price={2} img={urlImg} />
-      <Product title="Test1" price={2} img={urlImg} />
-      <Product title="Test1" price={2} img={urlImg} />
-      <Product title="Test1" price={2} img={urlImg} />
+      {contentBeans.map((item, index) => (
+        <Product
+          key={index}
+          title={item.title}
+          price={item.price}
+          img={item.img}
+        />
+      ))}
     </div>
   );
 }
